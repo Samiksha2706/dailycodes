ea2fd2ceff39fbbd40b6b77d5645dd3585b115ec
Samiksha2706 <74098831+Samiksha2706@users.noreply.github.com>
2023-06-11 23:07:59 +0530

    Add files via upload

---
+++ java1 ( stack and queue).java
@@ -0,0 +1,13 @@
+public class java1 ( stack and queue) {
+    public static void main(String[] args)
+    {
+        Stack<integer> stack = new Stack<>();
+        stack.push(34);
+        stack.push(36);
+
+        System.out.println(stack);
+
+
+    }
+
+}
